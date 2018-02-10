8b4aafa5c1caefd972216cec62e51f476b9c94c5
kaitoAckerman <k4170.4ck3rm4n@gmail.com>
2018-02-10 14:27:19 +0800

    Init zcss framework
    Setup all gulp plugin needed and customize gulpfile script

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
+
+var jsSrc = ['src/scripts/*.js'],
+	sassSrc = ['src/sass/*.scss'],
+	dist = 'dist';
+
+gulp.task('sass', function () {
+	gulp.src(sassSrc)
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.on('error', gutil.log)
+		.pipe(concat('app.min.css'))
+		.pipe(gulp.dest(dist))
+});
+
+gulp.task('js', function () {
+	gulp.src(jsSrc)
+		.pipe(uglify())
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest(dist))
+});
+
+gulp.task('watch', function () {
+	gulp.watch(jsSrc, ['js']);
+	gulp.watch(sassSrc, ['sass']);
+});
+
+gulp.task('default', ['js', 'sass', 'watch']);
