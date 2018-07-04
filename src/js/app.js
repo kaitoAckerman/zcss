faf17761b7916971078ec9ad9190f08497f3a69a
kaitoAckerman <4ck3rm4n@gmail.com>
2018-07-04 10:59:28 +0800

    Update new npm script
    urgent fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,1 @@
-$(document).ready(function () {
-	console.log("ready!");
-});
-
-$('[data-modal]').on('click', function (e) {
-	var modal = jQuery(this).attr('data-modal');
-	$('[data-content="' + modal + '"]').fadeIn(350);
-	e.preventDefault();
-});
-
-$('[data-close]').on('click', function (e) {
-	var modal = jQuery(this).attr('data-close');
-	$('[data-content="' + modal + '"]').fadeOut(350);
-	e.preventDefault();
-});
+console.log('test1');
