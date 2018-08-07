782ddd197ed3ceda722edb0e1cf6b8372f389321
kaitoAckerman <4ck3rm4n@gmail.com>
2018-08-07 12:08:31 +0800

    Zcss forked from milligram

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,36 @@
-console.log('test');
-
-// Get all elements with class="closebtn"
-var close = document.getElementsByClassName("closebtn");
-var i;
-
-// Loop through all close buttons
-for (i = 0; i < close.length; i++) {
-    // When someone clicks on a close button
-    close[i].onclick = function(){
-
-        // Get the parent of <span class="closebtn"> (<div class="alert">)
-        var div = this.parentElement;
-
-        // Set the opacity of div to 0 (transparent)
-        div.style.opacity = "0";
-
-        // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
-        setTimeout(function(){ div.style.display = "none"; }, 600);
+! function () {
+    function e(e) {
+        for (r = 0; r < n.length; r++) n[r].classList.remove("popover-open")
     }
-}
+
+    function t(t) {
+        t.preventDefault(), document.querySelector(this.getAttribute("href")).classList.contains("popover-open") ? document.querySelector(this.getAttribute("href")).classList.remove("popover-open") : (e(), document.querySelector(this.getAttribute("href")).classList.add("popover-open")), t.stopImmediatePropagation()
+    }
+    var o = document.querySelectorAll("[data-popover]"),
+        n = document.querySelectorAll(".popover"),
+        r = void 0;
+    for (r = 0; r < o.length; r++) o[r].addEventListener("click", t);
+    document.addEventListener("click", e)
+}(),
+function () {
+    var e = document.querySelectorAll(".code-content"),
+        t = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': "&quot;",
+            "'": "&#39;",
+            "/": "&#x2F;"
+        },
+        o = void 0;
+    for (o = 0; o < e.length; o++) e[o].innerHTML = function (e) {
+        return String(e).replace(/[&<>"']/g, function (e) {
+            return t[e]
+        })
+    }(e[o].innerHTML);
+    ! function (e, t, o, n, r, i) {
+        r = t.createElement(o), r.async = 1, r.src = "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js", i = t.getElementsByTagName(o)[document.querySelectorAll(o).length - 1], i.parentNode.insertBefore(r, i)
+    }(window, document, "script")
+}()
+
+console.log('test');
